f72d96c95afeaf5d5b6563432032d787600ba9a1
Arcturus91 <arturo.barrantesv@pucp.pe>
2022-12-31 17:03:44 -0500

    intersection types, guards, discriminated union

---
+++ src/app.js
@@ -0,0 +1,7 @@
+// Code goes here!
+var e1 = {
+    name: "arturo8",
+    privileges: ["createServer "],
+    startDate: new Date()
+};
+console.log(e1);
--- src/app.ts
+++ src/app.ts
@@ -5,25 +5,109 @@ type Admin = {
   privileges: string[];
 };
 
-type Employee =  {
-    name:string
-    startDate: Date //Date is a type supported in TS.
-}
+type Employee = {
+  name: string;
+  startDate: Date; //Date is a type supported in TS.
+};
 
 //interface ElevateEmployee extends Employee, Admin {}
 
-type ElevateEmployee = Admin & Employee 
+type ElevateEmployee = Admin & Employee;
 
-const e1:ElevateEmployee = {
-name:"arturo",
-privileges: ["createServer "],
-startDate: new Date()
+const e1: ElevateEmployee = {
+  name: "arturo8",
+  privileges: ["createServer "],
+  startDate: new Date(),
+};
 
+type Combinable = string | number; // union type here.
+
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric; // here we have the intersection type. It will look for the type that is in common of the two type above. Notice they are Union type.
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
 }
 
-type Combinable = string | number // union type here.
+type UnknownEmployee = Employee | Admin;
 
-type Numeric = number |  boolean
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log("Name: " + emp.name);
+  if ("privileges" in emp) {
+    console.log("privileges:" + emp.privileges);
+  }
 
-type Universal = Combinable & Numeric // here we have the intersection type. It will look for the type that is in common of the two type above. Notice they are Union type.
+  if ("startDate" in emp) {
+    console.log("startDate:" + emp.startDate);
+  }
+}
 
+printEmployeeInformation({ name: "Artur", startDate: new Date() });
+
+class Car {
+  drive() {
+    console.log("drive");
+  }
+}
+class Truck {
+  drive() {
+    console.log("driving");
+  }
+  loadCargo(amount: number) {
+    console.log("amount:" + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) /* :void */ {
+  //void for returning anything. You cant use Void.
+  vehicle.drive();
+
+  /* if("loadCargo" in vehicle){ it works
+  vehicle.loadCargo(1000)
+} */
+
+  //guard typeS:  Used for when you have intersection types
+  if (vehicle instanceof Truck) {
+    //and this works too. instanceof verify wether the argument is an instance of Truck which is the base class.
+    vehicle.loadCargo(200);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
+
+interface Bird {
+  type: "bird"
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type:"horse"
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+//instanceof will not work here because interfaces are tools of TS only. They disappear in JS.
+function moveAnimal(animal: Animal) {
+  let speed
+ switch (animal.type){
+  case "bird":
+    speed = animal.flyingSpeed
+    break
+    case "horse":
+      speed:animal.runningSpeed
+ }
+console.log("moving at speed" + speed)
+}
+//note we are forcing any instance to have the object property "type" (this because of the interface)
+//thus, because we are sure the instance will have the property type,  we can use it for conditionals in the function block
